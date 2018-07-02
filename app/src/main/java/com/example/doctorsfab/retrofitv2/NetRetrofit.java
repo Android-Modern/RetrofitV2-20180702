815dabd13992342a3877c15cb84e4fe8a12c5074
qwertyu5258 <qwertyu5258@naver.com>
2018-07-02 11:05:47 +0900

    1

---
+++ app/src/main/java/com/example/doctorsfab/retrofitv2/NetRetrofit.java
@@ -0,0 +1,28 @@
+package com.example.doctorsfab.retrofitv2;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class NetRetrofit {
+
+    private static NetRetrofit ourInstance = new NetRetrofit();
+
+    public static NetRetrofit getInstace() {
+        return ourInstance;
+    }
+
+    private NetRetrofit() {
+
+    }
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    RetrofitService service = retrofit.create(RetrofitService.class);
+
+    public RetrofitService getService() {
+        return service;
+    }
+}
---
+++ app/src/main/java/com/example/doctorsfab/retrofitv2/RetrofitService.java
@@ -0,0 +1,15 @@
+package com.example.doctorsfab.retrofitv2;
+
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface RetrofitService {
+ @GET("users/{user}/repos")
+    Call<ArrayList<JsonObject>> getListRepos(@Path("user") String id);
+
+}
